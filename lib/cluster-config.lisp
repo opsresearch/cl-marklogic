f31b5ecfb37910e10aef4df5df1aafe7e317b234
Donald Anderson <dranderson@opsresearch.com>
2016-02-02 07:42:55 -0500

    documentation

--- lib/cluster-config.lisp
+++ lib/cluster-config.lisp
@@ -21,37 +21,41 @@
 (in-package #:cl-marklogic)
 
 (defvar *cluster-config* nil
-  "The default global cached cluster config.
-  The macro with-cluster-config can be used to override the global cached cluster config.")
+  "This is the default global cached cluster config.
+  Use the macro with-cluster-config to wrap expressions that you want to use a different default configuration.")
 
 (defun get-cluster-config()
-  "Get the global cached cluster config."
+  "This funtion returns the global cached cluster config."
   *cluster-config*)
 
 (defun set-cluster-config(new-value)
-  "Set the global cached cluster cache."
+  "This function sets the value of the global cached cluster cache."
   (setf *cluster-config* new-value))
 
 (defmacro with-cluster-config ((cluster-config-value) &body body)
-  "Bind `cluster-config-value` to *cluster-config* to override the global cached cluster configuration."
+  "This macro binds `cluster-config-value` to *cluster-config* overriding the default cached cluster configuration for the wrapped expressions."
   `(let ((*cluster-config* ,cluster-config-value))
      (progn
        ,@body)))
 
 (defun write-cluster-config (to &optional (config (get-cluster-config)))
+  "This function writes a cluster configuration to a stream."
   (cond ((streamp to)  (write config :stream to :readably t))          
         (T (with-open-file (stream to :direction :output :if-exists :supersede)
                            (write config :stream stream :readably t)))))
 
 (defun read-cluster-config (from)
+  "This function reads a cluster configuration from a stream."
   (cond ((streamp from)  (read from))
         (T (with-open-file (stream from :direction :input)
                            (read stream)))))
 
 (defun cache-cluster-config ()
+  "This function refreshes the default cached cluster configuration from the default connection."
   (set-cluster-config (get-cluster-config-from-connection)))
 
 (defun get-cluster-config-from-connection ()
+  "This function fetches a fresh cluster configuration from the default connection."
   (with-cluster-config (nil)
                        (list
                          (cons :type "ccfg")
@@ -66,9 +70,11 @@
                          )))
 
 (defun cluster-config-property (property-name &optional (cluster-config *cluster-config*))
+  "This function accesses a property in a cluster configuration."
   (cdr (assoc property-name cluster-config)))
 
 (defun load-cluster-config (cluster-config-name)
+  "This function loads a cluster configuration from a project located in the system source tree into the default cached cluster configuration."
   (let ((config (read-cluster-config 
                   (merge-pathnames
                     (make-pathname :directory '(:relative "default-project") :name cluster-config-name :type "ccfg")
@@ -78,6 +84,7 @@
           (T config))))
 
 (defun save-cluster-config (&optional (cluster-config *cluster-config*))
+  "This function saves a cluster configuration to a project located in the system source tree from the default cached cluster configuration."
   (write-cluster-config 
     (merge-pathnames
       (make-pathname :directory '(:relative "default-project") :name (cluster-config-property :name cluster-config) :type "ccfg")
